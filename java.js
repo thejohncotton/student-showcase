dbd224cda7f5813cdf2d07d9ab2a601e86c6fbca
Christopher Chaney <CHANEYspeaks@gmail.com>
2018-02-12 16:41:18 -0500

    add photo

--- java.js
+++ java.js
@@ -1,4 +1,10 @@
 
 
+<script type="text/javascript">
+console.log('Hello world!');
+</script>
+<script type="text/javascript">
+var name = prompt('What is your name?');
 
-  
+alert('Hello ' + name);
+</script>
